9066c0960ea086a6fd237cae238901aefbf8940b
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2024-10-24 19:52:08 +1300

    oops ok this time its fully fixed i promis

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ text = "";
 with open("dist/index.html", "r") as f:
     text = f.read()
 
-text = text.replace("bindings from '/eframe_template.js'", "bindings from './webcamera/eframe_template.js'")
+text = text.replace("bindings from '/eframe_template.js'", "bindings from '/webcamera/eframe_template.js'")
 text = text.replace("const wasm = await init('/eframe_template_bg.wasm');", "const wasm = await init('/webcamera/eframe_template_bg.wasm');")
 text = text.replace("href=\"/eframe_template.js\"", "href=\"/webcamera/eframe_template.js\"")
 text = text.replace("href=\"/eframe_template_bg.wasm\"", "href=\"/webcamera/eframe_template_bg.wasm\"")

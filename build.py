27396c27dce155d7f54562ca6d0f6b74e4b8a43f
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2024-10-24 19:49:02 +1300

    hopefull fix v2

--- build.py
+++ build.py
@@ -6,7 +6,9 @@ text = "";
 with open("dist/index.html", "r") as f:
     text = f.read()
 
-text = text.replace("bindings from '/eframe_template.js'", "bindings from './eframe_template.js'")
+text = text.replace("bindings from '/eframe_template.js'", "bindings from './webcamera/eframe_template.js'")
+text = text.replace("const wasm = await init('/eframe_template_bg.wasm');", "const wasm = await init('/webcamera/eframe_template_bg.wasm');")
+text = text.replace("href=\"/eframe_template.js\"", "href=\"/webcamera/eframe_template.js\"")
 
 with open("dist/index.html", "w") as f:
     f.write(text)

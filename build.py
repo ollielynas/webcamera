9ae79a010022e066bc87dc5b8a32dd7cf19a7a86
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2024-10-24 20:10:08 +1300

    fix hopefully

--- build.py
+++ build.py
@@ -1,15 +1,15 @@
-import os
+# import os
 
-os.system("trunk build")
+# os.system("trunk build")
 
-text = "";
-with open("dist/index.html", "r") as f:
-    text = f.read()
+# text = "";
+# with open("dist/index.html", "r") as f:
+#     text = f.read()
 
-text = text.replace("bindings from '/eframe_template.js'", "bindings from '/webcamera/eframe_template.js'")
-text = text.replace("const wasm = await init('/eframe_template_bg.wasm');", "const wasm = await init('/webcamera/eframe_template_bg.wasm');")
-text = text.replace("href=\"/eframe_template.js\"", "href=\"/webcamera/eframe_template.js\"")
-text = text.replace("href=\"/eframe_template_bg.wasm\"", "href=\"/webcamera/eframe_template_bg.wasm\"")
+# text = text.replace("bindings from '/eframe_template.js'", "bindings from '/webcamera/eframe_template.js'")
+# text = text.replace("const wasm = await init('/eframe_template_bg.wasm');", "const wasm = await init('/webcamera/eframe_template_bg.wasm');")
+# text = text.replace("href=\"/eframe_template.js\"", "href=\"/webcamera/eframe_template.js\"")
+# text = text.replace("href=\"/eframe_template_bg.wasm\"", "href=\"/webcamera/eframe_template_bg.wasm\"")
 
-with open("dist/index.html", "w") as f:
-    f.write(text)
+# with open("dist/index.html", "w") as f:
+#     f.write(text)
